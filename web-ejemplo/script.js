27b16116c50d5053e3015374161339100f08e012
Diego Villalba <diegoo.villalba@gmail.com>
2022-01-29 16:03:21 -0300

    Primer de varios commits

---
+++ web-ejemplo/script.js
@@ -0,0 +1,36 @@
+console.log(restar(10,3));
+/*const list = [1,2,3];
+
+list.push(157);
+
+console.log(list);
+
+const persona = {
+    firstName : "Diego",
+    twitter : "V_Diego_",
+    age : 27,
+    links: ["https://www.linkedin.com/in/villalba-diego/", "https://twitter.com/V_Diego_"]
+}
+
+console.log(persona.age);
+console.log(persona.links[0]);
+
+const field = "twitter";
+
+console.log("ACA")
+console.log(persona[field]);
+*/
+const sumar = (a, b) => {
+    console.log(a);
+    console.log(b);
+    return a + b;
+}
+
+let op1 = 4;
+let op2 = 6;
+
+console.log(sumar(op1,op2));
+
+function restar(a,b) {
+    return a - b;
+}
